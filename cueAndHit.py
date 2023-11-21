1d2d3f6124726a30d4f569b58f7b878f70b6894a
Runqiu Ye <ye_runqiu@outlook.com>
2023-11-21 10:19:54 -0500

    implemented motion, add comment

--- cueAndHit.py
+++ cueAndHit.py
@@ -2,31 +2,20 @@ from cmu_graphics import *
 import math
 
 
+# Distance Helper Function
 def distance(x1, y1, x2, y2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
 
-class Ball:
-    pointDict = {
-        "red": 1,
-        "yellow": 2,
-        "green": 3,
-        "brown": 4,
-        "blue": 5,
-        "pink": 6,
-        "black": 7,
-    }
+# ========================================================================
+# Table functions (initialization, drawing)
+# ========================================================================
 
-    def __init__(self, color, cx, cy):
-        self.color = color
-        self.cx = cx
-        self.cy = cy
-        self.vx = 0
-        self.vy = 0
-        self.point = Ball.pointDict[self.color] if color != "white" else -1
-        self.radius = 5.3
+# Drawing and initlization functions
+# ========================================================================
 
 
+# Initialize table geometry, including pocket locations
 def initializeTableGeometry(app):
     app.width = 850
     app.height = 650
@@ -37,8 +26,17 @@ def initializeTableGeometry(app):
     app.lineLocation = 74 * 2
     app.regionRadius = 29.7 * 2
     app.pocketRadius = 8.6 / 2 * 2
+    app.pocketLocations = [
+        (app.tableCenterX + app.tableLength / 2, app.tableCenterY + app.tableWidth / 2),
+        (app.tableCenterX + app.tableLength / 2, app.tableCenterY - app.tableWidth / 2),
+        (app.tableCenterX - app.tableLength / 2, app.tableCenterY + app.tableWidth / 2),
+        (app.tableCenterX - app.tableLength / 2, app.tableCenterY - app.tableWidth / 2),
+        (app.tableCenterX, app.tableCenterY + app.tableWidth / 2),
+        (app.tableCenterX, app.tableCenterY - app.tableWidth / 2),
+    ]
 
 
+# draw play area (green mat)
 def drawPlayArea(app):
     length = app.tableLength
     width = app.tableWidth
@@ -74,42 +72,13 @@ def drawPlayArea(app):
     )
 
 
+# draw pocket
 def drawPocket(app):
-    length = app.tableLength
-    width = app.tableWidth
-    pocketRadius = app.pocketRadius
-    drawCircle(
-        app.tableCenterX + length / 2,
-        app.tableCenterY + width / 2,
-        pocketRadius,
-        fill="black",
-    )
-    drawCircle(
-        app.tableCenterX + length / 2,
-        app.tableCenterY - width / 2,
-        pocketRadius,
-        fill="black",
-    )
-    drawCircle(
-        app.tableCenterX - length / 2,
-        app.tableCenterY + width / 2,
-        pocketRadius,
-        fill="black",
-    )
-    drawCircle(
-        app.tableCenterX - length / 2,
-        app.tableCenterY - width / 2,
-        pocketRadius,
-        fill="black",
-    )
-    drawCircle(
-        app.tableCenterX, app.tableCenterY + width / 2, pocketRadius, fill="black"
-    )
-    drawCircle(
-        app.tableCenterX, app.tableCenterY - width / 2, pocketRadius, fill="black"
-    )
+    for pocketX, pocketY in app.pocketLocations:
+        drawCircle(pocketX, pocketY, app.pocketRadius, fill="black")
 
 
+# draw table border
 def drawTableBorder(app):
     length = app.tableLength * 1.05
     width = app.tableWidth * 1.1
@@ -118,16 +87,72 @@ def drawTableBorder(app):
     )
 
 
+# draw Snooker Table
 def drawSnookerTable(app):
     drawTableBorder(app)
     drawPlayArea(app)
     drawPocket(app)
 
 
+# ========================================================================
+# Snooker balls functions (class, intialization, drawing)
+# ========================================================================
+
+# class of snooker Balls
+# ========================================================================
+
+
+class Ball:
+    pointDict = {
+        "red": 1,
+        "yellow": 2,
+        "green": 3,
+        "brown": 4,
+        "blue": 5,
+        "pink": 6,
+        "black": 7,
+    }
+
+    def __init__(self, color, cx, cy):
+        self.color = color
+        self.cx = cx
+        self.cy = cy
+        self.vx = 0
+        self.vy = 0
+        self.point = Ball.pointDict[self.color] if color != "white" else -1
+        self.radius = 5.3
+
+    def move(self, app):
+        tableLeft = app.tableCenterX - app.tableLength / 2
+        tableRight = app.tableCenterX + app.tableLength / 2
+        tableTop = app.tableCenterY - app.tableWidth / 2
+        tableBot = app.tableCenterY + app.tableWidth / 2
+        self.cx += self.vx
+        self.cy += self.vy
+        if self.cx > tableRight - self.radius:
+            self.cx = tableRight - self.radius
+            self.vx = -self.vx
+        elif self.cx < tableLeft + self.radius:
+            self.cx = tableLeft + self.radius
+            self.vx = -self.vx
+        elif self.cy < tableTop + self.radius:
+            self.cy = tableTop + self.radius
+            self.vy = -self.vy
+        elif self.cy > tableBot - self.radius:
+            self.cy = tableBot - self.radius
+            self.vy = -self.vy
+
+
+# Drawing and initlization functions
+# ========================================================================
+
+
+# Intialize snooker balls
 def initializeBalls(app):
     app.whiteBall = Ball("white", app.tableCenterX, app.tableCenterY)
 
 
+# draw snooker balls
 def drawBalls(app):
     drawCircle(
         app.whiteBall.cx,
@@ -139,6 +164,11 @@ def drawBalls(app):
     )
 
 
+# ========================================================================
+# cue stick functions (drawing)
+# ========================================================================
+
+
 def drawCueStick(app, aimingDirection):
     ballRad = app.whiteBall.radius
     unitX = aimingDirection[0]
@@ -148,13 +178,12 @@ def drawCueStick(app, aimingDirection):
     startY = app.whiteBall.cy + 2 * ballRad * unitY
     endX = app.whiteBall.cx + cueLength * unitX
     endY = app.whiteBall.cy + cueLength * unitY
-    drawLine(startX, startY, endX, endY, lineWidth = 5)
+    drawLine(startX, startY, endX, endY, lineWidth=5)
 
 
-def onAppStart(app):
-    app.mouseX = 0
-    app.mouseY = 0
-    newGame(app)
+# ========================================================================
+# Game initializations (newGame)
+# ========================================================================
 
 
 def newGame(app):
@@ -162,22 +191,51 @@ def newGame(app):
     initializeBalls(app)
 
 
+# ========================================================================
+# Main functions (onAppStart, redrawAll, takeStep, onKeyPress)
+# ========================================================================
+
+
+def onAppStart(app):
+    app.mouseX = 0
+    app.mouseY = 0
+    app.aiming = False
+    newGame(app)
+
+
 def redrawAll(app):
     drawSnookerTable(app)
     drawBalls(app)
-    aimingDistance = distance(
-        app.mouseX, app.mouseY, app.whiteBall.cx, app.whiteBall.cy
-    )
-    aimingDirection = [
-        (app.whiteBall.cx - app.mouseX) / aimingDistance,
-        (app.whiteBall.cy - app.mouseY) / aimingDistance,
-    ]
-    drawCueStick(app, aimingDirection)
+    if app.aiming == True:
+        aimingDistance = distance(
+            app.mouseX, app.mouseY, app.whiteBall.cx, app.whiteBall.cy
+        )
+        aimingDirection = [
+            (app.whiteBall.cx - app.mouseX) / aimingDistance,
+            (app.whiteBall.cy - app.mouseY) / aimingDistance,
+        ]
+        drawCueStick(app, aimingDirection)
 
 
 def onMouseMove(app, mouseX, mouseY):
     app.mouseX = mouseX
     app.mouseY = mouseY
+
+
+# def onStep(app):
+#     takeStep(app)
+
+
+def takeStep(app):
+    app.whiteBall.move(app)
+
+
+def onKeyPress(app, key):
+    if key == "h":
+        app.whiteBall.vx = 5
+        app.whiteBall.vy = -3
+    if key == "s":
+        takeStep(app)
 
 
 def main():
